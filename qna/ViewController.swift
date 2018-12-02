26826dc8e68f0df269195b9d88a5b9a495bd5c8f
Yuwen Chiu <yuwenchiu@yuwende-macbook-pro.local>
2018-12-02 15:45:08 +0800

    Initial Commit

---
+++ qna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qna
+//
+//  Created by Yuwen Chiu on 2018/12/2.
+//  Copyright © 2018 YuwenChiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
